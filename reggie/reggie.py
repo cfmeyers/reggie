83c9e0484a807fc011efeb0a3d507b65e9c77f06
Collin Meyers <cfmeyers@gmail.com>
2019-08-07 22:19:34 -0400

    Add table parser

--- reggie/reggie.py
+++ reggie/reggie.py
@@ -1,3 +1,11 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+from typing import List, TextIO
+import re
+
+
+def collect_created_tables(text_stream: TextIO) -> List[str]:
+    tables = []
+    pattern = r'(?:table|view)\s+(\w+\.?\w*)'
+    return re.findall(pattern, text_stream.read(), re.IGNORECASE | re.MULTILINE)
--- tests/test_reggie.py
+++ tests/test_reggie.py
@@ -2,37 +2,92 @@
 # -*- coding: utf-8 -*-
 
 """Tests for `reggie` package."""
+from io import StringIO
 
 import pytest
 
-from click.testing import CliRunner
-
-from reggie import reggie
-from reggie import cli
+from reggie.reggie import collect_created_tables
 
 
 @pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def single_table_str() -> StringIO:
+    return StringIO(
+        """\
+    CREATE table kittens AS SELECT * FROM raw.kittens;
+"""
+    )
 
 
-def test_command_line_interface():
-    """Test the CLI."""
-    runner = CliRunner()
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert 'reggie.cli.main' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
-    assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
+@pytest.fixture
+def single_table_str_caps() -> StringIO:
+    return StringIO(
+        """\
+    CREATE TABLE kittens AS SELECT * FROM raw.kittens;
+"""
+    )
+
+
+@pytest.fixture
+def single_view_str_caps() -> StringIO:
+    return StringIO(
+        """\
+    CREATE VIEW kittens AS SELECT * FROM raw.kittens;
+
+"""
+    )
+
+
+@pytest.fixture
+def view_str_split_across_lines() -> StringIO:
+    return StringIO(
+        """\
+    CREATE VIEW 
+
+   kittens AS SELECT * FROM raw.kittens;
+
+"""
+    )
+
+
+@pytest.fixture
+def view_schema_and_table() -> StringIO:
+    return StringIO(
+        """\
+    CREATE VIEW fuzzy.bunnies AS SELECT * FROM raw.bunnies;
+"""
+    )
+
+
+@pytest.fixture
+def multiple_table_str() -> StringIO:
+    return StringIO(
+        """\
+    CREATE Table fuzzy.bunnies AS SELECT * FROM raw.bunnies;
+
+    CREATE VIEW kittens AS SELECT * FROM raw.bunnies;
+"""
+    )
+
+
+class TestCollectCreatedTables:
+    def test_it_parses_simple_table_statements(self, single_table_str):
+        assert ['kittens'] == collect_created_tables(single_table_str)
+
+    def test_it_parses_simple_table_statements_witout_case_sensitivity(
+        self, single_table_str_caps
+    ):
+        assert ['kittens'] == collect_created_tables(single_table_str_caps)
+
+    def test_it_parses_simple_view_statements(self, single_view_str_caps):
+        assert ['kittens'] == collect_created_tables(single_view_str_caps)
+
+    def test_it_parses_view_split_across_lines(self, view_str_split_across_lines):
+        assert ['kittens'] == collect_created_tables(view_str_split_across_lines)
+
+    def test_it_parses_schema_and_table_names(self, view_schema_and_table):
+        assert ['fuzzy.bunnies'] == collect_created_tables(view_schema_and_table)
+
+    def test_it_parses_multiple_table_statements(self, multiple_table_str):
+        assert ['fuzzy.bunnies', 'kittens'] == collect_created_tables(
+            multiple_table_str
+        )
